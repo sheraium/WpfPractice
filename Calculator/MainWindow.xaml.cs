d3f893503c739fb3b851f36936e0cf9989e26155
sheraium <sheraium@gmail.com>
2020-06-25 17:23:11 +0800

    add Calculator project

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var first = Convert.ToInt32(txtFirstNumber.Text);
+            var second = Convert.ToInt32(txtSecondNumber.Text);
+            lblSumResult.Content = $"{first} + {second} = {first + second}";
+            lblMinResult.Content = $"{first} - {second} = {first - second}";
+            lblDivResult.Content = $"{first} / {second} = {first / second}";
+            lblMulResult.Content = $"{first} * {second} = {first * second}";
+        }
+    }
+}
