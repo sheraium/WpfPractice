d25eb32ae304ad3660e64e110ad369e2f07bc03a
sheraium <sheraium@gmail.com>
2020-06-25 22:06:58 +0800

    add CreateButtons project

---
+++ CreateButtons/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CreateButtons
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            canvas1.Children.Clear();
+
+            var x = 20;
+            var y = 20;
+
+            for (var i = 0; i < x; i++)
+            {
+                for (var j = 0; j < y; j++)
+                {
+                    var button = new Button()
+                    {
+                        Width = 50,
+                        Height = 50,
+                    };
+
+                    Canvas.SetTop(button, j * 50);
+                    Canvas.SetLeft(button, i * 50);
+
+                    canvas1.Children.Add(button);
+                }
+            }
+        }
+    }
+}
