bf356a861720190b6e0c2f088dc2b7fc85390497
sheraium <sheraium@gmail.com>
2020-06-25 23:42:33 +0800

    add ZoomInOut project

---
+++ ZoomInOut/ViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace CustomControl
+{
+    public class ViewModel : ViewModelBase
+    {
+        private const decimal Unity = 1;
+
+        private decimal _scale = Unity;
+
+        public decimal Scale
+        {
+            get => _scale;
+            set => Set(ref _scale, value);
+        }
+
+        public decimal ScaleStep => 0.1m;
+
+        public decimal MinimumScale => 0.1m;
+
+        public decimal MaximumScale => 4.0m;
+
+        public ICommand ZoomInCommand => new RelayCommand(
+            () => Scale += ScaleStep, () => Scale < MaximumScale);
+
+        public ICommand ZoomOutCommand => new RelayCommand(
+            () => Scale -= ScaleStep, () => Scale > MinimumScale);
+
+        public ICommand ResetZoomCommand => new RelayCommand(
+            () => Scale = Unity, () => Scale != Unity);
+    }
+}
