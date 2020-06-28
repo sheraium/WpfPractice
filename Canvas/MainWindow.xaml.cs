1af6cf0569bf199b2d17dc5344605891951b1b0c
sheraium <sheraium@gmail.com>
2020-06-28 11:04:02 +0800

    Canvas, draw rectangle

--- Canvas/MainWindow.xaml.cs
+++ Canvas/MainWindow.xaml.cs
@@ -31,24 +31,51 @@ namespace Canvas
 
         private void Cnv_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            _draw = !_draw;
+            _draw = true;
             _x = e.GetPosition(cnv).X;
             _y = e.GetPosition(cnv).Y;
         }
 
         private void Cnv_OnMouseMove(object sender, MouseEventArgs e)
         {
-            if (_draw)
+            if (_draw && e.LeftButton == MouseButtonState.Pressed)
             {
-                var line = new Line();
-                line.X1 = _x;
-                line.Y1 = _y;
-                line.X2 = e.GetPosition(cnv).X;
-                line.Y2 = e.GetPosition(cnv).Y;
-                line.StrokeThickness = 2;
-                line.Stroke = Brushes.Brown;
-                cnv.Children.Add(line);
+                cnv.Children.Clear();
+                //var line = new Line();
+                //line.X1 = _x;
+                //line.Y1 = _y;
+                //line.X2 = e.GetPosition(cnv).X;
+                //line.Y2 = e.GetPosition(cnv).Y;
+                //line.StrokeThickness = 2;
+                //line.Stroke = Brushes.Brown;
+                //cnv.Children.Add(line);
+
+                var rectangle = new Rectangle();
+                var height = e.GetPosition(cnv).Y - _y;
+                var width = e.GetPosition(cnv).X - _x;
+                rectangle.Height = Math.Abs(height);
+                rectangle.Width = Math.Abs(width);
+                rectangle.StrokeThickness = 2;
+                rectangle.Stroke = Brushes.Brown;
+                var top = _y;
+                var left = _x;
+                if (height < 0)
+                {
+                    top = e.GetPosition(cnv).Y;
+                }
+                if (width < 0)
+                {
+                    left = e.GetPosition(cnv).X;
+                }
+                System.Windows.Controls.Canvas.SetTop(rectangle, top);
+                System.Windows.Controls.Canvas.SetLeft(rectangle, left);
+                cnv.Children.Add(rectangle);
             }
+        }
+
+        private void Cnv_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _draw = false;
         }
     }
 }
