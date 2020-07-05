9a821b1390e70f93f478d25a0e147a3ed2e59b0c
sheraium <sheraium@gmail.com>
2020-07-05 21:39:18 +0800

    add Globalization project

---
+++ Globalization/Cultures/CulturesHelper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Markup;
+
+public class CulturesHelper
+{
+    private static bool _isFoundInstalledCultures = false;
+
+    private static string _resourcePrefix = "StringResources";
+
+    private static string _culturesFolder = "Cultures";
+
+    private static List<CultureInfo> _supportedCultures = new List<CultureInfo>();
+
+    public static List<CultureInfo> SupportedCultures
+    {
+        get
+        {
+            return _supportedCultures;
+        }
+    }
+
+    public CulturesHelper()
+    {
+        if (!_isFoundInstalledCultures)
+        {
+            CultureInfo cultureInfo = new CultureInfo("");
+
+            List<string> files = Directory.GetFiles(string.Format("{0}\\{1}", System.Windows.Forms.Application.StartupPath, _culturesFolder))
+                .Where(s => s.Contains(_resourcePrefix) && s.ToLower().EndsWith("xaml")).ToList();
+
+            foreach (string file in files)
+            {
+                try
+                {
+                    string cultureName = file.Substring(file.IndexOf(".") + 1).Replace(".xaml", "");
+
+                    cultureInfo = CultureInfo.GetCultureInfo(cultureName);
+
+                    if (cultureInfo != null)
+                    {
+                        _supportedCultures.Add(cultureInfo);
+                    }
+                }
+                catch (ArgumentException)
+                {
+                }
+            }
+
+            if (_supportedCultures.Count > 0 && Globalization.Properties.Settings.Default.DefaultCulture != null)
+            {
+                ChangeCulture(Globalization.Properties.Settings.Default.DefaultCulture);
+            }
+
+            _isFoundInstalledCultures = true;
+        }
+    }
+
+    public static void ChangeCulture(CultureInfo culture)
+    {
+        if (_supportedCultures.Contains(culture))
+        {
+            string LoadedFileName = string.Format("{0}\\{1}\\{2}.{3}.xaml", System.Windows.Forms.Application.StartupPath, _culturesFolder
+                , _resourcePrefix, culture.Name);
+
+            FileStream fileStream = new FileStream(@LoadedFileName, FileMode.Open);
+
+            ResourceDictionary resourceDictionary = XamlReader.Load(fileStream) as ResourceDictionary;
+
+            Application.Current.MainWindow.Resources.MergedDictionaries.Add(resourceDictionary);
+
+            Globalization.Properties.Settings.Default.DefaultCulture = culture;
+            Globalization.Properties.Settings.Default.Save();
+        }
+    }
+}
